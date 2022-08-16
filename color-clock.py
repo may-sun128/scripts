de9e216682051d6b63ad6ac24b6489ed79c43e88
mholmes <masondaleholmes@gmail.com>
2022-08-16 13:08:35 +0000

    added colored clock

---
+++ color-clock.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import datetime
+import time
+from termcolor import colored
+
+colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan']
+
+running = True
+
+cnt = 0
+
+while(running):	
+	if cnt < len(colors): 	
+		print(colored(datetime.datetime.now(), colors[cnt]))
+		time.sleep(3)
+	else: 
+		cnt = -1 
+	cnt += 1
