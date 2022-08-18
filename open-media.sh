a63c3450f1433acc5c13e73eed68a14f79eef29c
mholmes <masondaleholmes@gmail.com>
2022-08-18 22:33:09 +0000

    fixed open-media so that it opens in a new window

---
+++ open-media.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+firefox --safe-mode --new-window https://www.tcm.com/watchtcm/livestream/ 
+sleep 5
+firefox --new-tab https://go.cnn.com/ --new-tab https://www.netflix.com/browse --new-tab https://www.wrkf.org/ --new-tab https://www.my90stv.com/#WgFoIr_Yu-M
