a57eeee12a9110945147c374c6003c6acb200a90
mholmes <masondaleholmes@gmail.com>
2022-08-19 11:00:51 +0000

    added timeout, refactored code

--- src/jiggler/jiggler.py
+++ src/jiggler/jiggler.py
@@ -9,7 +9,7 @@ minutes_before_jiggle = None
 # Check to make sure there's only one arg 
 # If not, set minutes_before_jiggle to 1
 
-# DEBUG: Leaving this out for now
+# Hard-coding arguments for now instead of getting them from the command line
 '''
 if ((len(sys.argv)<2) or sys.argv[1].isalpha() or int(sys.argv[1])<1):
     minutes_before_jiggle = 1
@@ -17,38 +17,29 @@ else:
     minutes_before_jiggle = int(sys.argv[1])
 '''
 
-class Debug:
-    def __init__(self):
-        self.temp = None
-
-    def LogSeconds(self):
-        i: int = 0
-        while i < 0:
-            time.sleep(1)
-            print(i)
-            i += 1
-
-    def dprint(self, s: str):
-        print(s)
-
-
-def jiggle(minutes_before_jiggle: int):
+def jiggle(minutes_before_jiggle: int, timeout_hour: str):
     running = True
-    while(running):
+    while running:
         # HH:MM:SS
         current_time: str = datetime.now().strftime('%X')
         # If it's after 12am, stop script 
-        if current_time.split(':')[0] == '0':
-            running = False
+        current_hour = current_time.split(':')[0]
+        if current_hour == timeout_hour:
+            # Apparently, setting running to false does not exit the while loop
+            # I guess it gets evaluated only at the beginning of the loop, since python is interpreted
+            #running = False
+            # Using the exit function instead--note sure if this is best practice
+            print('Timed out.')
+            exit()
         
-        # x = 0
-        # # Wait until time to jiggle 
-        # while(x < minutes_before_jiggle):
-        #     time.sleep(60)
-        #     x += 1
+        x = 0
+        # Wait until time to jiggle
+        while(x < minutes_before_jiggle):
+            time.sleep(60)
+            x += 1
 
         # Actually move mouse
-        for i in range(0,200):
+        for i in range(0,100):
             pyautogui.moveTo(0,i*4)
         pyautogui.moveTo(1,1)
 
@@ -60,7 +51,8 @@ def jiggle(minutes_before_jiggle: int):
         #print("Movement made at {}".format(datetime.now().time()))
 
 def main():
-    jiggle(1)
+    # start immediately (first arg), timeout at 1am (second arg)
+    jiggle(0, '01')
 
     db.dprint('Built')
 
