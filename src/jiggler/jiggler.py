1fe99a0308b95a3ab3e2b7a784d452f0888020be
mholmes <masondaleholmes@gmail.com>
2022-08-19 10:06:54 +0000

    cleaning up code a little

--- src/jiggler/jiggler.py
+++ src/jiggler/jiggler.py
@@ -2,12 +2,14 @@ import pyautogui
 import time
 import sys
 from datetime import datetime
+
 pyautogui.FAILSAFE = False
 numMin = None
 if ((len(sys.argv)<2) or sys.argv[1].isalpha() or int(sys.argv[1])<1):
     numMin = 1
 else:
     numMin = int(sys.argv[1])
+
 while(True):
     x=0
     while(x<numMin):
