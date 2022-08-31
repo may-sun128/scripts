830c4cb931b94c573e30ab3b779239eabaa8d97b
mholmes <masondaleholmes@gmail.com>
2022-08-31 20:05:40 +0000

    added rich text formatting

--- src/pathy/pathy.py
+++ src/pathy/pathy.py
@@ -3,6 +3,8 @@
 # This script displays a human-readible list of directories in PATH at the cmd
 
 import subprocess 
+from rich.console import Console
+from rich.table import Table 
 
 paths_subprocess_object = subprocess.Popen("echo $PATH", stdout=subprocess.PIPE, shell=True)
 binaryPaths, err = paths_subprocess_object.communicate()
@@ -14,6 +16,15 @@ paths = binaryPaths.decode('ascii')
 
 paths_list = paths.split(':') 
 
-print('***** PATHS ******')
+# print('***** PATHS ******')
+# for path in paths_list:
+#     print(path)
+
+table = Table("Global Paths")
+
 for path in paths_list:
-    print(path)
+    table.add_row(path)
+
+console = Console()
+console.print(table)
+
