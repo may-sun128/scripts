1559187e0c4129d9136f6f0229225b17bec63196
mholmes <masondaleholmes@gmail.com>
2022-09-09 10:45:00 +0000

    fixed sys argv

---
+++ timeout/arg-test/arg-test.py
@@ -0,0 +1,5 @@
+import sys 
+
+print(f'len of args = {len(sys.argv)}')
+for arg in sys.argv:
+	print(arg)
--- timeout/time-test_v3.py
+++ timeout/time-test_v3.py
@@ -13,47 +13,77 @@ def get_seconds_to_sleep():
 	return time_before_sleep_seconds
 
 def get_sleep_time():
-	if len(sys.argv) == 0:
-		# This is a dumb way of doing this, but it stands for right now. 
+	now  = datetime.now()
 
-		year = int(now.strftime('%Y')) # or %y?
-		month = int(now.strftime('%m')) 
-		day = int(now.strftime('%d')) + 1 # will not work on the last day of the month 
+	# This is a dumb way of doing this, but it stands for right now. 
+
+	year = int(now.strftime('%Y')) # or %y?
+	month = int(now.strftime('%m')) 
+	# NOTE, if note bebugging add + 1 to day (so it'll be after midnight)
+	day = int(now.strftime('%d')) + 1 # will not work on the last day of the month
+	second = int('15')
+
+	if len(sys.argv) == 1:
+		 
+		# hard code the sleep time if no argument is specified
 		hour = int('2')
 		minute = int('1')
+		
 
 		# time to go to sleep 
 		# year month day hour minute second
 		# hard-coding for now
-		sleep_time = datetime(year, month, day, hour, minute, 15)
+		sleep_time = datetime(year, month, day, hour, minute, second)
 
 		return sleep_time
 	else: 
-		time: str = sys.argv[0]
-		# time_l: list = time.split('') # 1, 2, 0, 0 for noon
-		# hour = time_l[0] + time_l[1]
-		dbug_print(sys.argv[0])
+		time: str = sys.argv[1]
+		
+		if time[0] != '0':
+			hour = int(time[0] + time[1])
+		elif time[0] == 0:
+			hour = int(time[1])
 
-def dbug_print(s):
-	print(s)
+		if time[2] != '0':
+			minute = int(time[3])
+		elif time[2] == 0:
+			minute = int(time[2] + time[3])
+		
+		#minute = time[2] + time[3]
 
-def dbug_main():
-	get_sleep_time()
+		sleep_time = datetime(year, month, day, hour, minute, second)
+		return sleep_time
 
 def main():
 	seconds_to_sleep = get_seconds_to_sleep()
 
 	#subprocess_object = subprocess.Popen(f'sleep {seconds_to_sleep} && systemctl suspend', stdout=subprocess.PIPE, shell=True)
 	
+	print(seconds_to_sleep)
 	time.sleep(seconds_to_sleep)
-	subprocess_object = subprocess.Popen(f'systemctl suspend', stdout=subprocess.PIPE, shell=True)
+
+	suspend_command = 'systemctl suspend'
+	debug_command = 'echo hello'
+
+	subprocess_object = subprocess.Popen(debug_command, stdout=subprocess.PIPE, shell=True)
 	binary_output, err = subprocess_object.communicate()
 
 	print(err)
 
-dbug_main()
+
 main()
 
+
+
+#***** Debuggin *****
+
+# def dbug_print(s):
+# 	print(s)
+
+# def dbug_main():
+# 	get_sleep_time()
+
+# dbug_main()
 
 
 
